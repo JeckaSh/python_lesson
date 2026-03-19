e890b71235b38c9c76cc1f52038f1293633c4e16
JeckaSh <jeckashevelkov@icloud.com>
2026-03-19 16:00:57 +0300

    learn string data type chapter 1

---
+++ chapter_9/2.py
@@ -0,0 +1,13 @@
+# Задание 2
+
+# Используя индексатор, дополните приведённый ниже код так, чтобы он вывел символ w.
+
+'''
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+
+print(s[])
+'''
+
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+
+print(s[-10])
