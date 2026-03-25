0c242089bf24b1eaa5a864b666e0193b6918f4d6
JeckaSh <jeckashevelkov@icloud.com>
2026-03-25 14:16:58 +0300

    learn string data type chapter 3

---
+++ chapter_9/36.py
@@ -0,0 +1,15 @@
+# Задание 36
+
+# Используя метод format(), дополните приведённый ниже код так, чтобы он вывел текст:
+
+# In 2010, someone paid 10k Bitcoin for two pizzas.
+
+'''
+s = 'In {0}, someone paid {1} {2} for two pizzas.'
+
+print()
+'''
+
+s = 'In {0}, someone paid {1} {2} for two pizzas.'.format('2010', '10k', 'Bitcoin')
+
+print(s)
---
+++ chapter_9/37.py
@@ -0,0 +1,19 @@
+# Задание 37
+
+# Используя f-строку, дополните приведённый ниже код так, чтобы он вывел текст:
+
+# In 2010, someone paid 10K Bitcoin for two pizzas.
+
+'''
+s = 'In {}, someone paid {} {} for two pizzas.'
+
+print()
+'''
+
+year = '2010'
+price = '10K'
+value = 'Bitcoin'
+
+s = f'In {year}, someone paid {price} {value} for two pizzas.'
+
+print(s)
